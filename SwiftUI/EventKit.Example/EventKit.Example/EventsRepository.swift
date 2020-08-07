4f00f79bc3af819718fd271a9b0d9274bfdf84f2
Filip Němeček <nemecek.filip@outlook.com>
2020-08-07 09:32:11 +0200

    Implemented selected calendars persistence in SwiftUI version.

--- SwiftUI/EventKit.Example/EventKit.Example/EventsRepository.swift
+++ SwiftUI/EventKit.Example/EventKit.Example/EventsRepository.swift
@@ -25,9 +25,10 @@ class EventsRepository: ObservableObject {
     @Published var events: [EKEvent]?
     
     private init() {
-        selectedCalendars = Set([eventStore.defaultCalendarForNewEvents].compactMap({ $0 }))
+        selectedCalendars = loadSelectedCalendars() ?? Set([eventStore.defaultCalendarForNewEvents].compactMap({ $0 }))
         
         $selectedCalendars.sink { [weak self] (calendars) in
+            self?.saveSelectedCalendars(calendars)
             self?.loadAndUpdateEvents()
         }.store(in: &subscribers)
         
@@ -36,6 +37,22 @@ class EventsRepository: ObservableObject {
                 self?.loadAndUpdateEvents()
                 
         }.store(in: &subscribers)
+    }
+    
+    private func loadSelectedCalendars() -> Set<EKCalendar>? {
+        if let identifiers = UserDefaults.standard.stringArray(forKey: "CalendarIdentifiers") {
+            let calendars = eventStore.calendars(for: .event).filter({ identifiers.contains($0.calendarIdentifier) })
+            guard !calendars.isEmpty else { return nil }
+            return Set(calendars)
+        } else {
+            return nil
+        }
+    }
+    
+    private func saveSelectedCalendars(_ calendars: Set<EKCalendar>?) {
+        if let identifiers = calendars?.compactMap({ $0.calendarIdentifier }) {
+            UserDefaults.standard.set(identifiers, forKey: "CalendarIdentifiers")
+        }
     }
     
     private func loadAndUpdateEvents() {
